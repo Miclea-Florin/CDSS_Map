51e93d7f0d5cc74baf82d9a7ac55c90bf8501fa5
Miclea-Florin <95932163+Miclea-Florin@users.noreply.github.com>
2024-06-26 14:57:44 +0300

    .

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,7 +1,7 @@
 
   self.addEventListener('install', (event) => {
     console.log('Service worker installing...');
-    // Cache list of essential files
+
     event.waitUntil(
       caches.open('v1').then((cache) => {
         return cache.addAll([
@@ -21,17 +21,17 @@
   self.addEventListener('fetch', function(event) {
     event.respondWith(
       caches.match(event.request).then(function(response) {
-        // Return the cached response if found, otherwise fetch from the network
+  
         return response || fetch(event.request);
       })
     );
   });
 
    
-   // push notifications to be implemented
+
   self.addEventListener('push', event => {
     console.log('Push notification received', event);
-    // Display a notification here
+
   });
   
 
